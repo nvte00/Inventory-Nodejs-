f0e298620a6cc00f5281ac0ae61073f541e4f603
= <=>
2020-08-17 20:37:48 +0700

    third

---
+++ server.real.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+
+const db = require("./app/index.js");
+const Product = db.product;
+const Export = db.export;
+const ExportProduct = db.export_product;
+
+const app = express();
+db.sequelize.sync({ force: false }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to  application." });
+});
+/////////////////////////////////
+require("./routes/routes")(app);
+require('./routes/routes.export')(app);
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
